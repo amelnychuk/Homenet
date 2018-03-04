1875f2d0f7072cc120599db8db40096713004258
Andrew Melnychuk <amelnychukoseen@gmail.com>
2018-03-03 16:07:23 -0800

    Successful test of google calender data driving sonos speaker and phillips hue lights

--- LightDispatcher.py
+++ LightDispatcher.py
@@ -5,7 +5,9 @@ import time
 
 from phue import Bridge
 from _utils import Color
+from gtts import gTTS
 from Calender import getEventColors
+import Speaker
 
 
 ##Testing connecting to the bridge
@@ -19,16 +21,31 @@ R,G,B = (0.48046875, 0.81640625, 0.28125)
 
 
 #fade on to orange in deci-seconds
-
-
-for eventColor in getEventColors():
+zone = Speaker.getZone()
+server = Speaker.HttpServer(8000)
+server.start()
+for eventTxt, eventColor in getEventColors():
     print eventColor
     command = {'transitiontime': 30,
                'on': True,
                'bri': 254,
                'xy': Color(eventColor).asCt()}
 
+    speech = gTTS(text=eventTxt, lang='en', slow=False)
+    mp3file = (eventTxt + ".mp3").replace(" ","")
+
+    speech.save(mp3file)
+    print ("Print saved file: {}".format(mp3file))
+    Speaker.play_file(zone, mp3file, port=8000)
+
 
     b.set_light("Hue color lamp 1", command)
     time.sleep(5)
 
+
+b.set_light("Hue color lamp 1", 'on', False)
+
+
+
+time.sleep(5)
+server.stop()
