15b0a848729270b4fa8aad039ef6b4a29ea53fae
Andrew Melnychuk <amelnychukoseen@gmail.com>
2018-03-05 20:08:09 -0800

    Event data structure to control lights and speakers.

---
+++ Event.py
@@ -0,0 +1,39 @@
+#This is a micro schedual to optimize time
+
+
+#Breakfast
+#clean up
+
+
+#Turn on water
+#Shampoo
+#Soap
+#Rinse
+#Dry
+#Shave
+#Brush Teeth
+#Hair
+#Clean up
+
+#Dress
+#Make bed
+
+#Pack Lunch
+#Reminders of the day
+
+#WORK
+
+#Eat dinner
+#Code
+#Exercise
+#Plan next day
+
+
+class event():
+
+    def __init__(self):
+        #color of lights
+        self._color = [1,1,1]
+        self._Instruction = ""
+        self._devices = []
+
