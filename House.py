e9af98188a9a5f93422c4e4b6084621c1d9a4762
Andrew Melnychuk <amelnychukoseen@gmail.com>
2018-03-07 18:35:42 -0800

    Fleshed out Zone object added data dir to house.

--- House.py
+++ House.py
@@ -11,16 +11,25 @@ class House(object):
     #maybe a singleton with http server
     #resources
     #control time
-    DataDir = os.path.join(os.getcwd(), "mp3")
+
 
     def __init__(self):
         pass
+
 
     def startServer(self):
         pass
 
     def stopServer(self):
         pass
+
+    @classmethod
+    def setStorage(cls):
+        cls.storage = os.path.join(os.getcwd(), "mp3")
+
+    @classmethod
+    def getStorage(cls):
+        return cls.storage
 
     def run(self):
         #start http server
@@ -31,8 +40,33 @@ class House(object):
 
 
 class Zone(House):
+    """
+    This has a collection of light and speaker names. Can load from Json config file.
+    """
     def __init__(self):
+        self._lights = []
+        self._speakers = []
+
+    def addSpeakers(self, speakerName):
+        """
+
+        Args:
+            speakerName: (str) or (list of str) adds speaker names to objects
+
+        Returns:
+
+        """
+        if type(speakerName) == str:
+            self._speakers.append(speakerName)
+        else:
+            self._speakers.extend(speakerName)
+
+    def getSpeakers(self):
         pass
+
+    def getLights(self):
+        pass
+
 
 class Schedual(House):
     def __init__(self):
