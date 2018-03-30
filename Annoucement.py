d9043fd17295a20695f79affb786785e66d4f3f3
Andrew <amelnychukoseen@gmail.com>
2018-03-29 18:33:45 -0700

    fixed progress function range, fixed __str__ and __repr__ self.name call

--- Annoucement.py
+++ Annoucement.py
@@ -50,11 +50,11 @@ class Announcement(Event):
         duration = self.getEnd() - self.getStart()
         if duration.seconds % 3600 // 60 > 1:
             amount = 4
-            for i in range(amount-1):
+            for i in range(1,amount):
                 newtime = duration / int(1 / ((i + 1) / float(amount)))
                 newtime = self.getStart() + newtime
-
-                msg = "{} is {} percent complete".format(self._name, (int(i+1/float(amount)) * 100))
+                progress = i/float(amount) * 100
+                msg = "{} is {} percent complete".format(self._name, int(progress))
 
                 self.makeJob(msg, newtime)
 
@@ -75,16 +75,16 @@ class Announcement(Event):
         Job.at(str_time).do(announce)
         """
     def announcements(self):
-        #self.begin()
+        self.begin()
         #self.warning(minutes=5)
         #self.warning(minutes=1)
         self.progress()
 
     def __str__(self):
-        return "Announce: {} starts at {} and ends at {}".format(self.name, self.getStart(asDateTime=False), self.getEnd(asDateTime=False))
+        return "Announce: {} starts at {} and ends at {}".format(self.getName(), self.getStart(asDateTime=False), self.getEnd(asDateTime=False))
 
     def __repr__(self):
-        return "<Announce:{} at {} until {}>".format(self.name, self.getStart(asDateTime=False), self.getEnd(asDateTime=False))
+        return "<Announce:{} at {} until {}>".format(self.getName(), self.getStart(asDateTime=False), self.getEnd(asDateTime=False))
 
 
 
